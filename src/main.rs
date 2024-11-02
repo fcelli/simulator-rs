b0a4b7ee7b1ce3b826e8b39faff51d2f806e6de0
fcelli <fedecel93@gmail.com>
2024-11-02 16:24:17 +0000

    feat: unit tests for Vector2D

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use body::Body;
-use utils::Vector2D;
-
 mod body;
 mod utils;
+
+use body::Body;
+use utils::Vector2D;
 
 fn main() {
     let body1 = Body::new(Vector2D::new(0.0, 0.0), Vector2D::new(1.0, 1.0), 1.0);
@@ -12,5 +12,8 @@ fn main() {
     let distance = body1.distance(&body2);
     println!("Distance between body1 and body2: {distance}");
     println!("Velocity of body1: {}", body1.velocity.to_str());
-    println!("Normalized velocity of body1: {}", body1.velocity.normalize().to_str());
+    println!(
+        "Normalized velocity of body1: {}",
+        body1.velocity.normalize().to_str()
+    );
 }
--- src/utils.rs
+++ src/utils.rs
@@ -33,12 +33,32 @@ impl Vector2D {
 
 use std::ops::{Add, Mul, Sub};
 
+impl Add for Vector2D {
+    type Output = Vector2D;
+    fn add(self, rhs: Vector2D) -> Vector2D {
+        Vector2D {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
 impl Add for &Vector2D {
     type Output = Vector2D;
     fn add(self, rhs: &Vector2D) -> Vector2D {
         Vector2D {
             x: self.x + rhs.x,
             y: self.y + rhs.y,
+        }
+    }
+}
+
+impl Sub for Vector2D {
+    type Output = Vector2D;
+    fn sub(self, rhs: Vector2D) -> Vector2D {
+        Vector2D {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
         }
     }
 }
@@ -53,6 +73,16 @@ impl Sub for &Vector2D {
     }
 }
 
+impl Mul<f64> for Vector2D {
+    type Output = Vector2D;
+    fn mul(self, scalar: f64) -> Vector2D {
+        Vector2D {
+            x: self.x * scalar,
+            y: self.y * scalar,
+        }
+    }
+}
+
 impl Mul<f64> for &Vector2D {
     type Output = Vector2D;
     fn mul(self, scalar: f64) -> Vector2D {
@@ -60,5 +90,65 @@ impl Mul<f64> for &Vector2D {
             x: self.x * scalar,
             y: self.y * scalar,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use approx::assert_abs_diff_eq;
+
+    fn setup_test_data() -> (Vector2D, Vector2D, Vector2D) {
+        (
+            Vector2D::new(0.0, 0.0),
+            Vector2D::new(3.0, 4.0),
+            Vector2D::new(1.0, 2.0),
+        )
+    }
+
+    #[test]
+    fn test_magnitude() {
+        let (v1, v2, v3) = setup_test_data();
+        assert_eq!(v1.magnitude(), 0.0);
+        assert_eq!(v2.magnitude(), 5.0);
+        assert_abs_diff_eq!(v3.magnitude(), 2.2360679, epsilon = 1e-7);
+    }
+
+    #[test]
+    fn test_normalize() {
+        let (mut v1, mut v2, mut v3) = setup_test_data();
+        v1 = v1.normalize();
+        v2 = v2.normalize();
+        v3 = v3.normalize();
+        assert_abs_diff_eq!(v1.magnitude(), 0.0, epsilon = 1e-7);
+        assert_abs_diff_eq!(v2.magnitude(), 1.0, epsilon = 1e-7);
+        assert_abs_diff_eq!(v3.magnitude(), 1.0, epsilon = 1e-7);
+    }
+
+    #[test]
+    fn test_add() {
+        let (v1, v2, v3) = setup_test_data();
+        let r = v1 + v2 + v3;
+        assert_eq!(r.x, 4.0);
+        assert_eq!(r.y, 6.0);
+    }
+
+    #[test]
+    fn test_sub() {
+        let (v1, v2, v3) = setup_test_data();
+        let r = v1 - v2 - v3;
+        assert_eq!(r.x, -4.0);
+        assert_eq!(r.y, -6.0);
+    }
+
+    #[test]
+    fn test_mul() {
+        let (v1, v2, _) = setup_test_data();
+        let r1 = v1 * 2.0;
+        let r2 = v2 * 2.0;
+        assert_eq!(r1.x, 0.0);
+        assert_eq!(r1.y, 0.0);
+        assert_eq!(r2.x, 6.0);
+        assert_eq!(r2.y, 8.0);
     }
 }
